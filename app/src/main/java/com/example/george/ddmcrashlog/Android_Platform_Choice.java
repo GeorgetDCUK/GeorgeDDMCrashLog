284058aad792afa3f3f56cc5a17afd742d9c7b2c
GeorgetDCUK <Geordiie99?>
2019-02-15 08:59:03 +0000

    Added Platform choices for both platforms lcp gro and hypo

---
+++ app/src/main/java/com/example/george/ddmcrashlog/Android_Platform_Choice.java
@@ -0,0 +1,13 @@
+package com.example.george.ddmcrashlog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Android_Platform_Choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android__platform__choice);
+    }
+}
---
+++ app/src/main/java/com/example/george/ddmcrashlog/Apple_Platform_Choice.java
@@ -0,0 +1,13 @@
+package com.example.george.ddmcrashlog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Apple_Platform_Choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apple__platform__choice);
+    }
+}
