e90815a92914ed3c9ba614c2699e9e9272492957
GeorgetDCUK <Geordiie99?>
2019-02-19 10:21:07 +0000

    hjdfj

--- app/src/main/java/com/example/george/ddmcrashlog/Android_bug_tracker2.java
+++ app/src/main/java/com/example/george/ddmcrashlog/Android_bug_tracker2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class Android_Bug_Tracker2 extends AppCompatActivity {
 
@@ -22,6 +23,13 @@ public class Android_Bug_Tracker2 extends AppCompatActivity {
     }
     public void LogOut(View view) {
         Intent intent = new Intent(this, CrashLog_LogIn.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
+    public void SaveAndroidCrashData(View view) {
+        Toast.makeText(this, "Saved Crash Data to Database", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, Android_Platform_Choice.class);
         startActivity(intent);
         overridePendingTransition(R.anim.left_in, R.anim.left_out);
     }
--- app/src/main/java/com/example/george/ddmcrashlog/Apple_Bug_Tracker2.java
+++ app/src/main/java/com/example/george/ddmcrashlog/Apple_Bug_Tracker2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class Apple_Bug_Tracker2 extends AppCompatActivity {
 
@@ -13,14 +14,21 @@ public class Apple_Bug_Tracker2 extends AppCompatActivity {
         setContentView(R.layout.activity_apple_bug_tracker2);
     }
 
-    public void BackArrowToApple1(View view) {
-        Intent intent = new Intent(this, Apple_Bug_Tracker.class);
+    public void BackArrowToAndroid1(View view) {
+        Intent intent = new Intent(this, Android_Bug_Tracker.class);
         startActivity(intent);
         overridePendingTransition(R.anim.left_in, R.anim.left_out);
         onBackPressed();
     }
     public void LogOut(View view) {
         Intent intent = new Intent(this, CrashLog_LogIn.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
+    public void SaveAppleCrashData(View view) {
+        Toast.makeText(this, "Saved Crash Data to Database", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, Apple_Platform_Choice.class);
         startActivity(intent);
         overridePendingTransition(R.anim.left_in, R.anim.left_out);
     }
