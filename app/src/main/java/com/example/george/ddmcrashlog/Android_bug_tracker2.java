a444b3a660343e84d583f4a4458e16b92e5eda5c
GeorgetDCUK <Geordiie99?>
2019-02-21 16:17:08 +0000

    UI Refinements

--- app/src/main/java/com/example/george/ddmcrashlog/Android_bug_tracker2.java
+++ app/src/main/java/com/example/george/ddmcrashlog/Android_bug_tracker2.java
@@ -1,15 +1,23 @@
 package com.example.george.ddmcrashlog;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class Android_Bug_Tracker2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_bug_tracker2);
     }
--- app/src/main/java/com/example/george/ddmcrashlog/OnboardingInfo.java
+++ app/src/main/java/com/example/george/ddmcrashlog/OnboardingInfo.java
@@ -1,18 +1,22 @@
 package com.example.george.ddmcrashlog;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.example.george.ddmcrashlog.utils.HideStatusBar;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class OnboardingInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_info);
-
-        HideStatusBar.HideStatus(this);
     }
 }
 
