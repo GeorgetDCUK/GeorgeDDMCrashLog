a9e7e888da87417ee840e068a1151b4c99adedc9
GeorgetDCUK <georget@diabetes.co.uk>
2019-04-05 13:00:10 +0100

    New Success state in bottom sheet dialog drawer

--- app/src/main/java/com/example/george/ddmcrashlog/Android_Bug_Tracker2.java
+++ app/src/main/java/com/example/george/ddmcrashlog/Android_Bug_Tracker2.java
@@ -1,15 +1,25 @@
 package com.example.george.ddmcrashlog;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class Android_Bug_Tracker2 extends AppCompatActivity {
+
+    private Dialog mEditBottomSheetDialog;
+    private View mEditBottomsheetView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +53,25 @@ public class Android_Bug_Tracker2 extends AppCompatActivity {
 
     // Saves the android crash data inputted
     public void SaveAndroidCrashData(View view) {
-        Toast.makeText(this, "Saved Crash Data to Database", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, Android_Platform_Choice.class);
+        if (mEditBottomSheetDialog == null) {
+            mEditBottomSheetDialog = new Dialog(this, R.style.MaterialDialogSheet);
+        }
+            mEditBottomSheetDialog.setCancelable(false);
+        if (mEditBottomsheetView == null) {
+            mEditBottomsheetView = this.getLayoutInflater().inflate(R.layout.drawer_component,
+                    null);
+        }
+
+        mEditBottomSheetDialog.setContentView(mEditBottomsheetView);
+        Objects.requireNonNull(mEditBottomSheetDialog.getWindow())
+                .setLayout(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+        mEditBottomSheetDialog.getWindow().setGravity(Gravity.BOTTOM);
+        mEditBottomSheetDialog.show();
+    }
+
+    public void ExitToMain(View view) {
+        Intent intent = new Intent(this, CrashLog_Main.class);
         startActivity(intent);
         overridePendingTransition(R.anim.left_in, R.anim.left_out);
     }
